eb775202d52613b868ad2fd642c6a70a9bf2ef38
Viktor Kyssa <viktor.kyssa@cation.ee>
2020-11-18 22:20:54 +0200

    rest server, hbs, layouts, render pages, models, views

--- node-express/routes/add.js
+++ node-express/routes/add.js
@@ -1,11 +1,21 @@
 const {Router} = require('express')
 const router = Router()
+const Course = require('../models/course')
 
 router.get('/', (req, res) => {
     res.render('add', {
         title: 'Add course',
         isAdd: true
     })
+})
+
+router.post('/', async (req, res) => {
+    const {title, price, img} = req.body
+    const course = new Course(title, price, img)
+    
+    await course.save()
+
+    res.redirect('/courses')
 })
 
 module.exports = router
