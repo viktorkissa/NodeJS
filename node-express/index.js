4eb15207b45841a1dd8526d98a4f80566646da3e
Viktor Kyssa <viktor.kyssa@cation.ee>
2020-11-24 20:33:05 +0200

    Orders

--- node-express/index.js
+++ node-express/index.js
@@ -3,19 +3,23 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const Handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 const cartRoutes = require('./routes/cart')
+const ordersRoutes = require('./routes/orders')
 const User = require('./models/user')
 const { text } = require('express')
 
 const app = express()
 
 const hbs = exphbs.create({
-    defaultLayout: 'main',
-    extname: 'hbs'
-})
+    defaultLayout: 'main', 
+    extname: 'hbs',
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+  });
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
@@ -37,6 +41,7 @@ app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/cart', cartRoutes)
+app.use('/orders', ordersRoutes)
 
 // app.get('/', (req, res) => {
 //     // res.status(200) // goes by default
